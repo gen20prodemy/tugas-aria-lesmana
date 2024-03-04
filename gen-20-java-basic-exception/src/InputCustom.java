7eade2f7168e20526c4e17e1b782ca5a9a362325
Aria Lesmana <32252675+ArieLsmn@users.noreply.github.com>
2024-03-04 19:03:49 +0700

    Commit update

--- gen-20-java-basic-exception/src/InputCustom.java
+++ gen-20-java-basic-exception/src/InputCustom.java
@@ -5,21 +5,25 @@ public class InputCustom {
 
 
         public static void cekInputAngka(int n) throws ExceptionInputSalah {
-            if(n<0)
+            if(n<=0)
                 throw new ExceptionInputSalah("input tdk valid");
         }
 
-        public static void main (String[] args) {
-            Scanner in = new Scanner(System.in);
-            try{
-                int n = in.nextInt();
-                cekInputAngka(n);
+        public static void main (String[] args) throws ExceptionInputSalah {
 
+            System.out.println("Masukkan input sembarang:");
+            Scanner in = new Scanner(System.in);
+
+            int n = in.nextInt();
+
+            try {
+                cekInputAngka(n);
             }
             catch (ExceptionInputSalah e){
-
+                System.out.println("Exception buatan tertangkap");
             }
 
+            cekInputAngka(n);//menimbulkan error
 
         }
     }
