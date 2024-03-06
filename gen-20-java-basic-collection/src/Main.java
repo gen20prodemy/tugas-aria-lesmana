177a74b7d1483d62985ac386594ebd4a073d5f97
Aria Lesmana <32252675+ArieLsmn@users.noreply.github.com>
2024-03-06 10:22:12 +0700

    Commit 2 update

--- gen-20-java-basic-collection/src/Main.java
+++ gen-20-java-basic-collection/src/Main.java
@@ -4,6 +4,23 @@ import java.util.*;
 
 public class Main {
 
+    public static List<String> buatList(String[] item){
+        List<String> barang = new ArrayList<String>();
+        barang = Arrays.asList(item);
+        return barang;
+    }
+
+    public static void printList(List<String> ls){
+        Iterator<String> lsIterator = ls.iterator();
+
+        System.out.print("ArrayList elements: ");
+        while(lsIterator.hasNext()) {
+            System.out.print(lsIterator.next());
+            System.out.print(", ");
+        }
+        System.out.println("\n");
+    }
+
     public static Map<Integer,String> buatMap(List<String> input){
 
         Map<Integer,String> mapped = new HashMap<Integer,String>();
@@ -19,97 +36,109 @@ public class Main {
 
     public static void main(String[] args) {
 
-        List<String> list1 = new ArrayList<String>();
-        list1.add("Apel");
+        String[] buah = {"Apel","Banana","Citrus","Delima","Elderberry"};
+        //List<String> list1 = new ArrayList<String>();
+        List<String> list1 = buatList(buah);
+        /*list1.add("Apel");
         list1.add("Banana");
         list1.add("Citrus");
         list1.add("Delima");
-        list1.add("Elderberry");
+        list1.add("Elderberry");*/
 
-        Iterator<String> list1Iterator = list1.iterator();
+        Scanner scan = new Scanner(System.in);
 
-        System.out.print("ArrayList elements: ");
-        while(list1Iterator.hasNext()) {
-            System.out.print(list1Iterator.next());
-            System.out.print(", ");
+
+        System.out.println("Menu: ");
+        System.out.println("1.Print daftar item");
+        System.out.println("2.Cari item");
+        System.out.println("3.Konversi dan print daftar item");
+        int select = scan.nextInt();
+        if(select==1) printList(list1);
+
+        if(select==2) {
+            scan.nextLine();
+            System.out.println("Cari item berdasarkan string: ");
+            String in = scan.nextLine();
+            if (list1.contains(in))
+                System.out.print("Item bernama " + in + " ada di index: " + list1.indexOf(in));
+
+            System.out.println("\n");
         }
-        System.out.println("\n");
+        if(select==3) {
+            System.out.print("List ke Hashset1 elements: ");
 
-        Set<String> hashset1 = new HashSet<String>(list1);
+            Set<String> hashset1 = new HashSet<String>(list1);
 
-        for(String x: list1) hashset1.add(x);//hashset menggunakan iterasi manual
+            for (String x : list1) hashset1.add(x);//hashset menggunakan iterasi manual
 
-        System.out.print("List ke Hashset1 elements: ");
-        for(String x: hashset1) {
-            System.out.print(x);
-            System.out.print(", ");
+            System.out.print("List ke Hashset1 elements: ");
+            for (String x : hashset1) {
+                System.out.print(x);
+                System.out.print(", ");
+            }
+            System.out.println("\n");
+
+            Set<String> hashset2 = new HashSet<String>(list1);
+            hashset2.addAll(list1);//hashset menggunakan method addall
+
+            System.out.print("List ke Hashset2 (addAll) elements: ");
+            for (String x : hashset2) {
+                System.out.print(x);
+                System.out.print(", ");
+            }
+            System.out.println("\n");
+
+
+            Set<String> treeset1 = new TreeSet<String>(list1);
+            treeset1.addAll(list1);
+
+            Iterator<String> tree1Iterator = treeset1.iterator();
+            System.out.print("List ke Treeset elements: ");
+            while (tree1Iterator.hasNext()) {
+                System.out.print(tree1Iterator.next());
+                System.out.print(", ");
+            }
+            System.out.println("\n");
+
+            List<String> list2 = new ArrayList<String>();
+            list2.addAll(hashset1);
+            System.out.print("HashSet ke List elements: ");
+            for (String x : list2) {
+                System.out.print(x);
+                System.out.print(", ");
+            }
+            System.out.println("\n");
+            List<String> list3 = new ArrayList<String>();
+            list3.addAll(treeset1);
+            System.out.print("TreeSet ke List elements: ");
+            for (String x : list3) {
+                System.out.print(x);
+                System.out.print(", ");
+            }
+            System.out.println("\n");
+
+            Map<Integer, String> map1 = buatMap(list1);
+
+            System.out.println("List ke Map elements:");
+            for (Map.Entry<Integer, String> y :
+                    map1.entrySet()) {
+
+                // Printing keys
+                System.out.print(y.getKey() + ":");
+                System.out.println(y.getValue());
+            }
+            System.out.println();
+
+            Vector<String> vector1 = new Vector<String>(list1);
+            System.out.println("List ke Vector elements: " + vector1);
+            Vector<String> vector2 = new Vector<String>(hashset1);
+            System.out.println("HashSet ke Vector elements: " + vector2);
+            System.out.println();
+
+            Queue<String> queue1 = new LinkedList<>(list1);
+            System.out.print("List ke Queue elements: " + queue1);
+            //System.out.print("Contoh hasil method queue peek: "+ queue1.peek());
+            System.out.println("\n");
         }
-        System.out.println("\n");
-
-        Set<String> hashset2 = new HashSet<String>(list1);
-        hashset2.addAll(list1);//hashset menggunakan method addall
-
-        System.out.print("List ke Hashset2 (addAll) elements: ");
-        for(String x: hashset2) {
-            System.out.print(x);
-            System.out.print(", ");
-        }
-        System.out.println("\n");
-
-        List<String> list2 = new ArrayList<String>();
-        list2.addAll(hashset1);
-        System.out.print("HashSet ke List elements: ");
-        for(String x: list2) {
-            System.out.print(x);
-            System.out.print(", ");
-        }
-        System.out.println("\n");
-
-
-        Set<String> treeset1 = new TreeSet<String>(list1);
-        treeset1.addAll(list1);
-
-        Iterator<String> tree1Iterator = treeset1.iterator();
-        System.out.print("List ke Treeset elements: ");
-        while(tree1Iterator.hasNext()) {
-            System.out.print(tree1Iterator.next());
-            System.out.print(", ");
-        }
-        System.out.println("\n");
-
-        List<String> list3 = new ArrayList<String>();
-        list3.addAll(treeset1);
-        System.out.print("TreeSet ke List elements: ");
-        for(String x: list3) {
-            System.out.print(x);
-            System.out.print(", ");
-        }
-        System.out.println("\n");
-
-        Queue<String> queue1 = new LinkedList<>(list1);
-        System.out.print("List ke Queue elements: ");
-
-        while (!queue1.isEmpty()) {
-            System.out.print(queue1.poll()+", ");
-        }
-        System.out.println("\n");
-
-        Map <Integer,String> map1 = buatMap(list1);
-
-        System.out.println("List ke Map elements:");
-        for (Map.Entry<Integer, String> y :
-                map1.entrySet()) {
-
-            // Printing keys
-            System.out.print(y.getKey() + ":");
-            System.out.println(y.getValue());
-        }
-        System.out.println();
-
-        Vector<String> vector1 = new Vector<String>(list1);
-        System.out.println("List ke Vector elements: " + vector1);
-        Vector<String> vector2 = new Vector<String>(hashset1);
-        System.out.println("HashSet ke Vector elements: " + vector2);
-
     }
 }
