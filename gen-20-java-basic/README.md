2a491813d379015214284720780f8fa4ace7c986
Aria Lesmana <32252675+ArieLsmn@users.noreply.github.com>
2024-03-08 14:41:54 +0700

    Commit upload tugas lama

---
+++ gen-20-java-basic/README.md
@@ -0,0 +1,18 @@
+program dasar java untuk menampilkan hello world
+```
+public class Main { 
+
+    public static void main(String[] args) { 
+
+        System.out.print("Hello!");     
+        System.out.println("Hello world!");
+    }
+}
+```
+`public class Main` = deklarasi class java, biasanya nama Main digunakan untuk class yang menjalankan fungsi utama program
+
+`public static void main(String[] args)` = deklarasi fungsi buatan, yang disebut sebagai method, khusus untuk fungsi "public static void main" digunakan sebagai fungsi utama untuk me-run sebuah project java
+
+`System.out.print("Hello!");` = digunakan untuk menampilkan teks ke console
+
+`System.out.println("Hello world!");` = digunakan untuk melakukan print teks ke console disertai newline
---
+++ gen-20-java-basic/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main { //deklarasi class java, biasanya nama Main digunakan untuk class yang menjalankan fungsi utama program
+
+    public static void main(String[] args) { /* deklarasi fungsi buatan, yang disebut sebagai method,
+                                            khusus untuk fungsi "public static void main" digunakan sebagai fungsi utama
+                                            untuk me-run sebuah project java, dalam kurung terdapat parameter berupa
+                                            "String[] args","String[]" berupa tipe data variabel
+                                            args berupa nama dari variabel argumen dalam parameter */
+
+        System.out.print("Hello!");     /* fungsi System.out.println digunakan untuk melakukan print suatu teks ke console */
+        System.out.println("Hello world!");/* fungsi System.out.println digunakan untuk melakukan print suatu teks ke
+                                            console disertai newline, jika teks dalam argumen berisi "Hello world!",
+                                            maka "Hello world!" akan ditampilkan di console
+                                            setiap fungsi java yang dipanggil diakhiri dengan titik koma
+                                           sedangkan deklarasi kelas, fungsi atau loop menggunakan kurung kurawal */
+    }
+}
